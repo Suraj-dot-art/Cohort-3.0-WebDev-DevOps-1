4a8fc4a362fb39110908944719f23680804b69f1
Suraj <surajdivekarsd27@gmail.com>
2025-01-03 22:36:01 +0530

    added week 2

---
+++ Week-02 JavaScript/some Functions/functions.js
@@ -0,0 +1,15 @@
+//Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+
+
+// function isPalindrome(str) {
+//     return true;
+// }
+  
+
+//Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+// function isPalindrome(str) {
+//     return true;
+//   }
+  
+//   module.exports = isPalindrome;
+
